57df4d0d7cd62569719e53581cb580c1133fa5fa
meran <amr.bin.an@gmail.com>
2023-04-27 15:15:28 +0800

    fitur hapus Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -55,32 +55,22 @@ class ProductController extends Controller
     ]);
   }
 
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
   public function update(ProductRequest $request, $id)
   {
-    $data = $request->all(); // Semua validasi dari request berada dalam ProductRequest yak
-    $data['slug'] = Str::slug($request->name); // terkait slug STR ini, bisa baca di dokumentasi laravelnya langsung
+    $data = $request->all();
+    $data['slug'] = Str::slug($request->name);
 
     $item = Product::findOrFail($id);
     $item->update($data);
-    
+
     return redirect()->route('products.index');
   }
 
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
   public function destroy($id)
   {
-      //
+    $item = Product::findOrFail($id);
+    $item->delete();
+
+    return redirect()->route('products.index');
   }
 }
